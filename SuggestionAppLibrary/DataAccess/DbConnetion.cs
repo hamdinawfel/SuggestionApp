bc371602f23f0260bab31a6d0ff972669d4c2621
hamdinawfel <hamdii.nawfel@gmail.com>
2022-09-19 21:53:46 +0100

    build : setup up the database connection configurations

--- SuggestionAppLibrary/DataAccess/DbConnetion.cs
+++ SuggestionAppLibrary/DataAccess/DbConnetion.cs
@@ -1,6 +1,30 @@
-﻿namespace SuggestionAppLibrary.DataAccess
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace SuggestionAppLibrary.DataAccess
 {
-    internal class DbConnetion
+    public class DbConnetion
     {
+        private readonly IConfiguration _config;
+        private readonly IMongoDatabase _db;
+        private string _connectionId = "MongoDB";
+
+        public string DbName { get; private set; }
+        public string CategoryCollectionName { get; private set; } = "categories";
+        public string StatusCollectionName { get; private set; } = "statuses";
+        public string UserCollectionName { get; private set; } = "users";
+        public string SuggestionsCollectionName { get; private set; } = "suggestions";
+
+        public MongoClient Client { get; private set; }
+        public IMongoCollection<CategoryModel> CategoryCollection { get; set; }
+        public IMongoCollection<SuggestionModel> StatusCollection { get; set; }
+        public IMongoCollection<UserModel> UserCollection { get; set; }
+        public IMongoCollection<SuggestionModel> SuggestionCollection { get; set; }
+
+        public DbConnetion(IConfiguration config)
+        {
+            _config = config;
+            Client = new MongoClient(_config.GetConnectionString(_connectionId));
+        }
     }
 }
