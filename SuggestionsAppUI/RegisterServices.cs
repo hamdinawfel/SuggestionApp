be1ceb0034e32c92eb8729b61a0fd2aa19ad7847
hamdinawfel <hamdii.nawfel@gmail.com>
2022-09-19 15:31:03 +0100

    refactor : configure dependency injection to separate file

---
+++ SuggestionsAppUI/RegisterServices.cs
@@ -0,0 +1,11 @@
+﻿namespace SuggestionsAppUI
+{
+    public static class RegisterServices
+    {
+        public static void ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+        }
+    }
+}
