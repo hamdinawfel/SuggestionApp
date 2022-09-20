55b49a557b13b7d460418480f92e0a8dfa0f14f0
hamdinawfel <hamdii.nawfel@gmail.com>
2022-09-20 14:36:01 +0100

    build : add dependency injection to register database and collections

--- SuggestionsAppUI/RegisterServices.cs
+++ SuggestionsAppUI/RegisterServices.cs
@@ -1,4 +1,6 @@
-﻿namespace SuggestionsAppUI
+﻿using SuggestionAppLibrary.DataAccess;
+
+namespace SuggestionsAppUI
 {
     public static class RegisterServices
     {
@@ -7,6 +9,12 @@
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddMemoryCache();
+
+            builder.Services.AddSingleton<IDbConnetion, DbConnetion>();
+            builder.Services.AddSingleton<ICategoryData, MongoCategoryData>();
+            builder.Services.AddSingleton<IStatusData, MongoStatusData>();
+            builder.Services.AddSingleton<IUserData, MongoUserData>();
+            builder.Services.AddSingleton<ISuggestionData, MongoSuggestionData>();
         }
     }
 }
