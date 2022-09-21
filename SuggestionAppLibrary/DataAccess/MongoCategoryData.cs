cb333848715573f2062b0fee4f1cf201b7f3104a
hamdinawfel <hamdii.nawfel@gmail.com>
2022-09-21 09:50:09 +0100

    refactor : remove unused modules

--- SuggestionAppLibrary/DataAccess/MongoCategoryData.cs
+++ SuggestionAppLibrary/DataAccess/MongoCategoryData.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Caching.Memory;
-using MongoDB.Driver;
-
+﻿using Microsoft.Extensions.Caching.Memory;
 namespace SuggestionAppLibrary.DataAccess
 {
     public class MongoCategoryData : ICategoryData
