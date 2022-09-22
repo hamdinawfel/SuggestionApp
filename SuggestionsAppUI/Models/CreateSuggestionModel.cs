7dc8182519a8451e8a6e8030c7355ec9697fee44
hamdinawfel <hamdii.nawfel@gmail.com>
2022-09-22 11:49:44 +0100

    build: create model for create a suggestion and add validation inputs

---
+++ SuggestionsAppUI/Models/CreateSuggestionModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuggestionsAppUI.Models
+{
+    public class CreateSuggestionModel
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Suggestion { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [Display(Name = "Category")]
+        public string CategoryId { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; set; }
+    }
+}
