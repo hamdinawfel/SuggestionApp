c817d02f66b23929b28fc5907270d1a642dfc7b4
hamdinawfel <hamdii.nawfel@gmail.com>
2022-09-23 14:35:27 +0100

    build : add AddAuthentication service to registerServices using dependency injection

--- SuggestionsAppUI/RegisterServices.cs
+++ SuggestionsAppUI/RegisterServices.cs
@@ -1,4 +1,6 @@
-﻿using SuggestionAppLibrary.DataAccess;
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using SuggestionAppLibrary.DataAccess;
 
 namespace SuggestionsAppUI
 {
@@ -9,6 +11,11 @@ namespace SuggestionsAppUI
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddMemoryCache();
+
+            builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
+
+
 
             builder.Services.AddSingleton<IDbConnetion, DbConnetion>();
             builder.Services.AddSingleton<ICategoryData, MongoCategoryData>();
