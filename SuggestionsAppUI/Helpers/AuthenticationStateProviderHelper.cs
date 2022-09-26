cd294b360a2076650e89dd419500a7c6eb543c09
hamdinawfel <hamdii.nawfel@gmail.com>
2022-09-26 22:49:18 +0100

    refactor : extract the get authenticaed user logic to separate class to improve the reusability

---
+++ SuggestionsAppUI/Helpers/AuthenticationStateProviderHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using SuggestionAppLibrary.DataAccess;
+
+namespace SuggestionsAppUI.Helpers
+{
+    public static class AuthenticationStateProviderHelper
+    {
+        public static async Task<UserModel> GetUserFromAuth(
+            this AuthenticationStateProvider provider, IUserData userData)
+        {
+            var authState = await provider.GetAuthenticationStateAsync();
+            string objectId = authState.User.Claims.FirstOrDefault(c => c.Type.Contains("objectidentifier"))?.Value;
+            return await userData.GetUserFromAuthentication(objectId);
+        }
+    }
+}
